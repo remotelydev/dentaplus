bdc0c30770330c8d1d1dfbcedc5bac33e5452c6c
remotelydev <trzosoo@gmail.com>
2024-01-21 14:23:32 +0100

    WIP Contact

--- slices/Contact/index.vue
+++ slices/Contact/index.vue
@@ -9,7 +9,7 @@ defineProps(
     "index",
     "slices",
     "context",
-  ])
+  ]),
 );
 </script>
 
@@ -18,26 +18,23 @@ defineProps(
     id="#kontakt"
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
-    class="bg-gray-100"
-  >
+    class="bg-gray-100">
     <div class="container mx-auto py-12 px-4">
       <PrismicRichText :field="slice.primary.header" />
       <div class="flex mt-6">
         <div
           class="flex-1 basis-1/2"
           v-for="(item, i) in slice.items"
-          :key="`address-${i}`"
-        >
-          <PrismicRichText :field="slice.items[i].place" />
-          <PrismicRichText :field="slice.items[i].address" wrapper="address" />
-          <a class="block" :href="`tel:${slice.items[i].phone}`">
-            Tel.:{{ slice.items[i].phone }}
+          :key="`address-${i}`">
+          <PrismicRichText :field="item.place" />
+          <PrismicRichText :field="item.address" wrapper="address" />
+          <a class="block" :href="`tel:${item.phone}`">
+            Tel.:{{ item.phone }}
           </a>
           <a
             class="block underline decoration-denta-green"
-            :href="`http://www.google.com/maps/place/${slice.items[i].location.latitude},${slice.items[i].location.longitude}`"
-            target="_blank"
-          >
+            :href="`http://www.google.com/maps/place/${item.location.latitude},${item.location.longitude}`"
+            target="_blank">
             Jak dojechać »
           </a>
         </div>
