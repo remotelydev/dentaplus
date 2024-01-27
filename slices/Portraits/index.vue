88807d28c5a86316d985422f6522319c933f1b19
remotelydev <trzosoo@gmail.com>
2024-01-27 15:28:23 +0100

    WIP persona dialog

--- slices/Portraits/index.vue
+++ slices/Portraits/index.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { type Content } from "@prismicio/client";
+
+const dialog = ref();
+const visible = ref(false);
+const currentPersona = ref();
+
+const showPersona = (src, alt) => {
+  dialog.value?.showModal();
+  currentPersona.value = { src, alt };
+  visible.value = true;
+};
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
@@ -9,7 +20,7 @@ defineProps(
     "index",
     "slices",
     "context",
-  ])
+  ]),
 );
 </script>
 
@@ -17,20 +28,24 @@ defineProps(
   <section
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
-    class="container mx-auto"
-  >
+    class="container mx-auto">
     <div class="max-w-7xl mx-auto flex flex-wrap justify-around gap-4 shrink">
       <Portrait
         v-for="(image, i) in slice.items"
-        class="shrink"
+        :key="image.name"
+        class="shrink cursor-pointer"
         :class="i === 0 ? 'w-1/3 ml-24' : i === 1 ? 'w-1/3 mr-24' : 'w-1/6'"
         :portrait="image.portrait.url"
         :alt="image.portrait.alt"
         :name="image.name"
         :role="image.role"
         :index="i"
-        :key="image.name"
-      />
+        @click="showPersona(image.portrait.url, image.portrait.alt)" />
     </div>
+    <dialog ref="dialog" @close="visible = false">
+      <form v-if="visible" method="dialg">
+        <NuxtImg :src="currentPersona.src" :alt="currentPersona.alt" />
+      </form>
+    </dialog>
   </section>
 </template>
