c635c88842bbf4a6f2ddac87a6115a8c0f25c495
remotelydev <trzosoo@gmail.com>
2024-02-20 16:32:42 +0100

    Make contact page more responsive

--- slices/Contact/index.vue
+++ slices/Contact/index.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
+import { useResizeObserver, useWindowSize } from '@vueuse/core'
+
+import FacebookIcon from '../public/facebook.svg';
+import InstagramIcon from '../public/instagram.svg';
 import { type Content } from "@prismicio/client";
+
 const settings = useSettings();
+
+const contact = ref<HTMLElement | null>(null);
+const mapWidth = ref<string>('');
+const mapHeight = ref<string>('');
+const { width: windowWidth } = useWindowSize()
+
+useResizeObserver(contact, (entries) => {
+  const entry = entries[0];
+  const { width } = entry.contentRect;
+
+  if (windowWidth.value < 768) {
+    mapWidth.value = String(width);
+    mapHeight.value = String(Math.floor(width * 2 / 3))
+  } else if (windowWidth.value < 1280) {
+    mapWidth.value = String(Math.floor(width / 2));
+    mapHeight.value = String(Math.floor(width / 3))
+  } else {
+    mapWidth.value = String(Math.floor(width / 3));
+    mapHeight.value = String(Math.floor(width * 2 / 9))
+  }
+})
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
@@ -20,21 +46,39 @@ defineProps(
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    <div class="container mx-auto">
-      <div class="my-8 sm:flex sm:items-center sm:justify-between md:justify-center">
-        <div class="m-4 sm:grow md:grow-0 md:w-80 md:max-w-80">
+    <div
+      ref="contact"
+      class="container mx-auto xl:flex xl:justify-around xl:p-8"
+    >
+      <div class="my-8 md:flex md:items-center md:justify-center xl:flex-col xl:w-1/3 ">
+        <div class="m-4 sm:grow sm:max-w-72 md:grow-0 md:w-80 md:max-w-80 xl:w-full xl:grow ">
           <PrismicRichText :field="slice.items[0].place" />
-          <PrismicRichText :field="slice.items[0].address" wrapper="address" />
+          <div class="flex items-center basis-1/3 gap-2 my-2 md:basis-1">
+            <a href="https://www.instagram.com/klinika.dentaplus" target="_blank">
+              <InstagramIcon class="w-6 h-6" />
+            </a>
+            <a href="https://www.facebook.com/dentaplusturek" target="_blank">
+              <FacebookIcon class="w-6 h-6" />
+            </a>
+          </div>
+          <p class="font-bold">Godziny otwarcia:</p>
+          <p>Pn-Pt: 8:00 - 20:00</p>
+          <p>Sb: 10:00 - 15:00</p>
+          <PrismicRichText
+            class="my-2"
+            :field="slice.items[0].address"
+            wrapper="address"
+          />
           <a class="block" :href="`tel:${settings?.data.phone_turek}`">
-            Tel.:{{ settings?.data.phone_turek }}
+            +48 {{ settings?.data.phone_turek }}
           </a>
           <a class="block" :href="`mailto:${settings?.data.email_turek}`">
             {{ settings?.data.email_turek }}
           </a>
         </div>
         <iframe
-          width="320"
-          height="240"
+          :width="mapWidth"
+          :height="mapHeight"
           class="mx-auto sm:mx-0 border-0"
           allowfullscreen="true"
           loading="lazy"
@@ -43,20 +87,35 @@ defineProps(
         />
       </div>
 
-      <div class="my-8 sm:flex sm:items-center sm:justify-between md:justify-center">
-        <div class="m-4 sm:grow md:grow-0 md:w-80 md:max-w-80">
+      <div class="my-8 md:flex md:items-center md:justify-center xl:flex-col xl:w-1/3">
+        <div class="m-4 sm:max-w-72 md:grow-0 md:w-80 md:max-w-80 xl:w-full xl:grow ">
           <PrismicRichText :field="slice.items[1].place" />
-          <PrismicRichText :field="slice.items[1].address" wrapper="address" />
+          <div class="flex items-center basis-1/3 gap-2 my-2 md:basis-1">
+            <a href="https://www.instagram.com/klinika.dentaplus" target="_blank">
+              <InstagramIcon class="w-6 h-6" />
+            </a>
+            <a href="https://www.facebook.com/dentapluspoddebice" target="_blank">
+              <FacebookIcon class="w-6 h-6" />
+            </a>
+          </div>
+          <p class="font-bold">Godziny otwarcia:</p>
+          <p>Pn-Pt: 8:00 - 20:00</p>
+          <p>Sb: 10:00 - 15:00</p>
+          <PrismicRichText
+            class="my-2"
+            :field="slice.items[1].address"
+            wrapper="address"
+          />
           <a class="block" :href="`tel:${settings?.data.phone_poddebice}`">
-            Tel.:{{ settings?.data.phone_poddebice }}
+            +48 {{ settings?.data.phone_poddebice }}
           </a>
           <a class="block" :href="`mailto:${settings?.data.email_poddebice}`">
             {{ settings?.data.email_poddebice }}
           </a>
         </div>
         <iframe
-          width="320"
-          height="240"
+          :width="mapWidth"
+          :height="mapHeight"
           class="mx-auto sm:mx-0 border-0"
           allowfullscreen="true"
           loading="lazy"
